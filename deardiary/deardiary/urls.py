99fe5242d30398a107cc58050695b735afd603ad
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-04-25 16:59:31 -0700

    Added the views.py file and created url paths and corresponding views for the homepage and about page

--- deardiary/deardiary/urls.py
+++ deardiary/deardiary/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("about/", views.about),
+    path("", views.homepage), #as this is the homepage, there is no route/url path to take so the string is empty
 ]
