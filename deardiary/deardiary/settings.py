05efc3758cdb873bd3dc51de714d069072da1f56
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-04-30 16:50:09 -0700

    Add HTML templates

--- deardiary/deardiary/settings.py
+++ deardiary/deardiary/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "deardiary.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- deardiary/deardiary/views.py
+++ deardiary/deardiary/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse;
+from django.shortcuts import render; # to render templates, you need to import the render module
 
 def homepage(request):
-    return HttpResponse('homepage')
+    # return HttpResponse('homepage')
+    return render(request,'homepage.html')
+    
 
 def about(request):
-    return HttpResponse('about')
+    # return HttpResponse('about')
+    return render(request,'about.html')
