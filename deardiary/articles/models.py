a53c0830c0830cc9de2fc8dc5b8b46b404680ed3
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-05-01 21:41:00 -0700

    Add models for article

--- deardiary/articles/models.py
+++ deardiary/articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)  # each of these are columns in the db/elements of the article. Need to set them equal to their Field type
+    slug = models.SlugField()  # Slug is a URL
+    body = models.TextField()  
+    date = models.DateTimeField(auto_now_add=True)  # when an article is created, the time is automatically generated  
+    # later add: thumbnail, author
+    # thumbnail = models.ImageField
+    # author = models.CharField
