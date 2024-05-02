c826a28f5d38964a1f8cc4ec70120d12de3a23a4
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-05-01 22:16:48 -0700

    Created template tag to show article content

--- deardiary/articles/views.py
+++ deardiary/articles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render;
+from .models import Article;
 
 # Create your views here.
 def article_list(request):
-    return render(request, 'articles/article_list.html')
+    articles = Article.objects.all().order_by('date')  #this grabs all records from article db table. Can order by any field in the model
+    return render(request, 'articles/article_list.html', { 'articles': articles })  #the third parameter is the data we want to send to the template
