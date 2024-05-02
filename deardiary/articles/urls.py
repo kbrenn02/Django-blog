0aa4f67914d9889866bfdbdb16339c988a49f04f
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-05-02 16:54:12 -0700

    Extending templates, url parameters, and named urls

--- deardiary/articles/urls.py
+++ deardiary/articles/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    path("", views.article_list), #as this is the homepage, there is no route/url path to take so the string is empty
+    path("", views.article_list, name="list"), #as this is the homepage, there is no route/url path to take so the string is empty
+    path(r'<slug:slug>/', views.article_detail, name="detail"), #we've captured where the user wants to go to
 ]
--- deardiary/articles/views.py
+++ deardiary/articles/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render;
 from .models import Article;
+from django.http import HttpResponse
 
 # Create your views here.
 def article_list(request):
     articles = Article.objects.all().order_by('date')  #this grabs all records from article db table. Can order by any field in the model
     return render(request, 'articles/article_list.html', { 'articles': articles })  #the third parameter is the data we want to send to the template
+
+def article_detail(request, slug):
+    return HttpResponse(slug)
