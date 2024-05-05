4817b9a3076aa1df62c6ce1def32d8131f19c210
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-05-04 19:39:39 -0700

    Made a 'create article' page that is protected from unlogged in users

--- deardiary/articles/urls.py
+++ deardiary/articles/urls.py
@@ -5,5 +5,6 @@ app_name = 'articles'
 
 urlpatterns = [
     path("", views.article_list, name="list"), #as this is the homepage, there is no route/url path to take so the string is empty
+    path('create/', views.article_create, name='create'),
     path(r'<slug:slug>/', views.article_detail, name="detail"), #we've captured where the user wants to go to
 ]
--- deardiary/articles/views.py
+++ deardiary/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render;
 from .models import Article;
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def article_list(request):
@@ -10,3 +11,7 @@ def article_list(request):
 def article_detail(request, slug):
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', {'article': article})
+
+@login_required(login_url='/accounts/login/') #this protects the article_create view
+def article_create(request):
+    return render(request, 'articles/article_create.html')
