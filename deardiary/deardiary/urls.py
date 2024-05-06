38650d571ad5b7be03805c5335ff8737a10ddcc8
Kevin Brennan <kevinmbrennan08@gmail.com>
2024-05-06 15:56:02 -0700

    Redirect homepage to articles list

--- deardiary/deardiary/urls.py
+++ deardiary/deardiary/urls.py
@@ -21,11 +21,12 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns #this is used to render static files
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import views as article_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("about/", views.about),
-    path("", views.homepage), #as this is the homepage, there is no route/url path to take so the string is empty
+    path("", article_views.article_list, name='home'), #as this is the homepage, there is no route/url path to take so the string is empty
     path("articles/", include('articles.urls')),
     path('accounts/', include('accounts.urls')),
 ]
